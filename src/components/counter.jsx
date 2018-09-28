640531a36dbb8e47dda897bc927ac9db431ed42c
adeluleye <adelu4real@gmail.com>
2018-09-29 00:56:43 +0100

    rendering lists using map

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 10
+    count: 10,
+    tags: ['tag1', 'tag2', 'tag3']
   };
 
   styles = {
@@ -17,6 +18,11 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
