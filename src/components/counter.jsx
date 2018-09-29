11e1c043726d3c6f2b12511014082ef20488785c
adeluleye <adelu4real@gmail.com>
2018-09-30 00:05:55 +0100

    raising and handling events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -37,7 +37,7 @@ class Counter extends Component {
     console.log(this.props);
     return (
       <div>
-        {this.props.children}
+        <h4>{this.props.id}</h4>
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
@@ -46,6 +46,12 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={this.props.onDelete}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
         {this.renderTags()}
       </div>
