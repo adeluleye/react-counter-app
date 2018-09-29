ddb2d233a3a2d27c3c6037613ef110d165e107c1
adeluleye <adelu4real@gmail.com>
2018-09-30 00:31:25 +0100

    Removing the local state to achieve single source of truth

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,6 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-    tags: ['tag1', 'tag2', 'tag3']
-  };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-
   styles = {
     fontSize: 10,
     fontWeight: 'bold'
@@ -28,11 +11,6 @@ class Counter extends Component {
   //     this.handleIncrement = this.handleIncrement.bind(this);
   //   }
 
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <div>
@@ -41,7 +19,7 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -52,19 +30,18 @@ class Counter extends Component {
         >
           Delete
         </button>
-        {this.renderTags()}
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = 'badge m-2 badge-';
-    classes += this.state.value === 0 ? 'warning' : 'primary';
+    classes += this.props.counter.value === 0 ? 'warning' : 'primary';
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? 'Zero' : value;
   }
 }
